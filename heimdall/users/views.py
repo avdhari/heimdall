c8b1e9cf8678f4497b9b374c4a53a56472923dda
Aravind Hari <avdhari114@gmail.com>
2022-03-17 12:00:50 +0000

    home view and url config

--- heimdall/users/views.py
+++ heimdall/users/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import DetailView, RedirectView, UpdateView
+from django.views.generic import DetailView, RedirectView, UpdateView, ListView
+from django.shortcuts import render
 
 User = get_user_model()
 
@@ -43,3 +44,9 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 
 user_redirect_view = UserRedirectView.as_view()
+
+
+class HomeView(ListView):
+    model = User
+    template_name = 'pages/home.html'
+    queryset = User.objects.all()
