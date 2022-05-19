02838a01ce96cdeafcb8c4d36d1bafac3ee506e4
Aravind Hari <avdhari114@gmail.com>
2022-05-19 15:06:23 +0530

    Scraped Data model update

--- heimdall/athena/models.py
+++ heimdall/athena/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from heimdall.users.models import Product
 
 
 class BaseModel(models.Model):
@@ -10,8 +11,11 @@ class BaseModel(models.Model):
 
 
 class ScrapedData(BaseModel):
+    """Model to store web scraped data"""
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     title = models.CharField(max_length=255)
     source = models.CharField(max_length=300)
     raw_data = models.TextField()
     text_data = models.TextField()
     generated_on = models.DateTimeField(auto_now_add=True)
+
