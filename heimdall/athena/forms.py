9559c7b23c439f770444a55358bad32334dac941
Aravind Hari <avdhari114@gmail.com>
2022-06-25 10:44:55 +0530

    ScrapedData form and template

--- heimdall/athena/forms.py
+++ heimdall/athena/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from heimdall.athena.models import ScrapedData
+from heimdall.users.models import Product
 
 
 class NewDataForm(forms.ModelForm):
     class Meta:
         model = ScrapedData
         fields = ('product', 'source')
+
+    def __init__(self, *args, **kwargs):
+        cmp_id = kwargs.pop('cmp_id')
+        super(NewDataForm, self).__init__(*args, **kwargs)
+        self.fields['product'].queryset = Product.objects.filter(company_id=cmp_id)
--- heimdall/athena/views.py
+++ heimdall/athena/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from heimdall.athena.forms import NewDataForm
 from heimdall.athena.utils import get_raw_data
 
 
 def home_view(request):
-    return render(request, 'athena/home.html')
+    if request.method == "POST":
+        new_data_form = NewDataForm(request.POST, cmp_id=request.user.company_id)
+        if new_data_form.is_valid():
+            new_data_form.save()
+            return redirect('/')
+    new_data_form = NewDataForm(cmp_id=request.user.company_id)
+
+    context = {
+        'new_data_form': new_data_form,
+    }
+    return render(request, 'athena/home.html', context)
